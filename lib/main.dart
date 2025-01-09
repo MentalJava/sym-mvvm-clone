b75f20e350f6789904908fb8c40431a61d5cff81
Si Yujun <siuj0810@gmail.com>
2025-01-09 16:09:52 +0900

    초기 Calendar 작업 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-import 'package:sym_mvvm_clone/ui/component/s_button.dart';
-import 'package:sym_mvvm_clone/ui/component/s_color.dart';
-import 'package:sym_mvvm_clone/ui/component/s_text.dart';
-import 'package:sym_mvvm_clone/ui/component/s_typo.dart';
+import 'package:sym_mvvm_clone/ui/home/home_view.dart';
 import 'package:sym_mvvm_clone/ui/sign_in/sign_in_view.dart';
 import 'package:sym_mvvm_clone/ui/sign_up/sign_up_view.dart';
 import 'package:sym_mvvm_clone/ui/splash/splash_view.dart';
@@ -15,6 +13,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   String? kakaoNativeAppKey = dotenv.env['KAKAO_API_KEY'];
   KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
+  await initializeDateFormatting();
   runApp(const MyApp());
 }
 
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      initialRoute: '/splash',
+      initialRoute: '/home',
       getPages: [
         GetPage(
           name: '/splash',
@@ -34,7 +33,7 @@ class MyApp extends StatelessWidget {
         ),
         GetPage(
           name: '/home',
-          page: () => const MyHomePage(),
+          page: () => HomeView(),
         ),
         GetPage(
           name: '/signIn',
@@ -45,48 +44,6 @@ class MyApp extends StatelessWidget {
           page: () => SignUpView(),
         )
       ],
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const SText(
-          text: 'hi',
-          type: TextType.header1,
-          color: SColor.black,
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SButton(
-              state: ButtonState.standard,
-              onCilcked: () {},
-              text: '완료',
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const SButton(
-              state: ButtonState.disabled,
-              text: '완료',
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/ui/home/home_view_model.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class HomeViewModel extends GetxController {
+  var focusedDay = DateTime.now().obs;
+  var selectedDay = DateTime.now().obs;
+  var selectedYear = DateTime.now().year.obs;
+  var selectedMonth = DateTime.now().month.obs;
+
+  void selectDay(DateTime newFocusedDay, DateTime newSeletedDay) {
+    selectedDay.value = newSeletedDay;
+    focusedDay.value = newFocusedDay;
+  }
+}
